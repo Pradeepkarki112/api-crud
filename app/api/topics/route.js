4c0f9368a7fd714e29fb83d0d557203774086339
Pradeep Kakri <pradeepkarki112@gmail.com>
2023-12-26 08:46:50 +0545

    POST function added

---
+++ app/api/topics/route.js
@@ -0,0 +1,10 @@
+import Topic from "@/models/topic"
+import { NextResponse } from "next/server"
+import connectMongoDB from "./../../../src/libs/mongodb"
+
+export async function POST(request) {
+  const { title, description } = await request.json()
+  await connectMongoDB()
+  await Topic.create({ title, description })
+  return NextResponse.json({ message: "Topic Created " }, { status: 201 })
+}
